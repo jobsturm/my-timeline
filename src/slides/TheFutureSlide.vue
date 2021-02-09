c05d55f3ba5991399fc8ca748acba27e016480ea
Job Sturm <job@feedbackcompany.com>
2021-02-09 12:52:54 +0100

    Added Closing Text

--- src/slides/TheFutureSlide.vue
+++ src/slides/TheFutureSlide.vue
@@ -1,7 +1,22 @@
 <template>
     <Slide class="slide" ref="slide" :background="colors.$marine_dark">
+        <div class="slide__centerer">
+            <main class="slide__content">
+                <h1 class="slide__title">Job's looking for a job!</h1>
+                <p class="slide__message">
+                    So if you are looking for a good frontend-developer
+                    and you think one of these timelines
+                    should contain a partnership,
+                    feel free to send me a message on LinkedIn!
+                    Furthermore, this project is open source, so if you want to look
+                    at how I work and build projects, you shoud check out the GitHub repo.
+                </p>
+                <a title="Job Sturm's LinkedIn Page" role="button" href="https://www.linkedin.com/in/job-sturm/">My LinkedIn</a>
+                <a title="Job Sturm's Portfolio's repository" role="button" href="https://github.com/jobsturm/portfolio-2020/">The repo</a>
+            </main>
+        </div>
         <TheFutureGraphic
-            class="illustration"
+            class="slide__illustration"
             :animationPercentage="entered"
             :start="timelinePosition.start"
             :end="end"
@@ -65,10 +80,28 @@ export default class TheFutureSlide extends SlideMixin {
         background: main.$black
         position: relative
 
-    .slide__illustration
+    .slide__illustration,
+    .slide__centerer
         position: absolute
         width: 100vw
         @include main.viewportHeight(100, 0)
         top: 0
         left: 0
+    .slide__centerer
+        z-index: 2
+        display: flex
+        justify-content: center
+        align-items: center
+
+    .slide__content
+        max-width: 600px
+        padding: 8px
+
+    .slide__title
+        @extend %headline3_style
+        color: main.$white
+        margin-bottom: 16px
+    .slide__message
+        @extend %body1_style
+        color: main.$white
 </style>
